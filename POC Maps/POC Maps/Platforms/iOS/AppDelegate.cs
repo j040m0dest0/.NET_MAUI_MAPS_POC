adb9ee6f74847e214ab65cd88f1778ba67aea798
joaom <joaom@DESKTOP-DT2GM4U>
2023-05-09 15:46:20 -0300

    first commit projects creation

---
+++ POC Maps/POC Maps/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace POC_Maps;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
